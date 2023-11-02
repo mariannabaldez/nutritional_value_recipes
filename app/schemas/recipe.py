ffaedd01281ef4a99919b28e587a17a800bd372f
mariannabaldez <mariannabaldez@gmail.com>
2023-11-01 22:15:14 -0300

    Rota para inserir receita e ingredientes d receita

---
+++ app/schemas/recipe.py
@@ -0,0 +1,49 @@
+#pylint: disable=E0213
+
+import re
+from pydantic import BaseModel, constr, conlist, Field, validator
+from typing import Dict, Optional
+from unidecode import unidecode
+
+
+class Recipe(BaseModel):
+    """ Modelo de receita culinária """
+    name: constr(
+        max_length=50,
+        strip_whitespace=True,
+        to_lower=True,
+        min_length=1,
+    ) = Field(..., example="empadao de frango")
+
+    descript: Optional[constr(
+        max_length=4000,
+        strip_whitespace=True,
+        min_length=1,
+    )] = Field(None, example= \
+            "Em uma panela, aqueça o óleo e refogue o arroz " + \
+            "até que os grãos estejam esbranquiçados, após " + \
+            "acrescente a cebola, o alho e refogue. " + \
+            "Adicione a água, acrescente o sal e espere " + \
+            "pela fervura para abaixar o fogo para médio. " + \
+            "Após a água reduzir e o arroz estiver quase " + \
+            "seco, tampe a panela e deixe o fogo no minimo " + \
+            "por 5 minutos e em seguida desligue o fogo."
+        )
+
+    ingredients: Optional[
+        Dict[str, Dict[str, int]]
+    ] = Field(..., example={
+            "farinha de trigo": {"gramas": 300},
+            "ovo": {"unidade": 3},
+            "manteiga": {"colheres": 3},
+            "peito de frango desfiado": {"gramas": 600},
+        })
+
+    @validator("name")
+    def apply_unidecode(cls, v):
+        """ Aplica unidecode no nome """
+        return re.sub(r'[^\w\s]', '', unidecode(v))
+
+class RecipeResponse(Recipe):
+    """ Modelo de resposta de receita culinária """
+    id: int
